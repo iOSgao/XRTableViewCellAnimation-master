83686738afb3dc8499b794a2ee49b5b49867fa61
apple <1754410821@qq.com>
2016-03-07 11:05:53 +0800

    commit empty project
    提交空工程

---
+++ XRTableViewCellAnimation-masterTests/XRTableViewCellAnimation_masterTests.swift
@@ -0,0 +1,36 @@
+//
+//  XRTableViewCellAnimation_masterTests.swift
+//  XRTableViewCellAnimation-masterTests
+//
+//  Created by xuran on 16/3/7.
+//  Copyright © 2016年 X.R. All rights reserved.
+//
+
+import XCTest
+@testable import XRTableViewCellAnimation_master
+
+class XRTableViewCellAnimation_masterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
